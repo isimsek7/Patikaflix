9b693cbcb4b853103bc8114224ea08b1e2e2e3f3
isimsek7 <isimsekmusic@gmail.com>
2024-09-03 15:58:04 +0300

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Patikaflix
+{
+	public class Dizi
+	{
+		public Dizi(string diziAdi,string yapimYili, string tur, string yayinTarihi, string yonetmen, string ilkPlatform)
+		{
+			_diziAdi = diziAdi;
+			_yapimYili = yapimYili;
+			_tur = tur;
+			_yayinTarihi = yayinTarihi;
+			_yonetmen = yonetmen;
+			_ilkPlatform = ilkPlatform;
+		}
+		public string _diziAdi { get; set; }
+		public string _yapimYili { get; set; }
+		public string _tur { get; set; }
+		public string _yayinTarihi { get; set; }
+		public string _yonetmen { get; set; }
+		public string _ilkPlatform { get; set; }
+	}
+	public class KomediDiziler
+	{
+		public KomediDiziler()
+		{
+		}
+		public string komediDiziAdi { get; set; }
+		public string komediTur { get; set; }
+		public string komediYonetmen { get; set; }
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Patikaflix;
+List<Dizi> Diziler = new List<Dizi>();
+bool count = true;
+Console.WriteLine("Bir dizi ekleyiniz");
+while(count)
+{
+    Console.Write("Dizi Adi= ");
+    string ad = Console.ReadLine();
+    Console.Write("Yapim Yili= ");
+    string yapimYili = Console.ReadLine();
+    Console.Write("Turu= ");
+    string tur = Console.ReadLine();
+    Console.Write("Yayin Tarihi= ");
+    string yayinTarihi = Console.ReadLine();
+    Console.Write("Yonetmen= ");
+    string yonetmen = Console.ReadLine();
+    Console.Write("Yayinlandigi Ilk Platform= ");
+    string plat = Console.ReadLine();
+    Dizi yeniDizi = new Dizi(ad, yapimYili, tur, yayinTarihi, yonetmen, plat);
+    Diziler.Add(yeniDizi);
+    bool count2 = true;
+    while (count2)
+    {
+        try
+        {
+            Console.WriteLine("Baska bir dizi eklemek ister misiniz ? E/H =");
+            string immo = Console.ReadLine().ToLower();
+            if (immo == "h")
+            {
+                count2 = false;
+                count = false;
+                Console.WriteLine("Girdiginiz diziler kaydedildi.");
+            }
+
+            else if (immo == "e")
+            {
+                count2 = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Character please try Again");
+
+            }
+        }
+        catch(FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+var DiziFinal = Diziler.Where(dizi => dizi._tur.Contains("Komedi", StringComparison.OrdinalIgnoreCase))
+    .Select(p => new KomediDiziler { komediDiziAdi = p._diziAdi, komediTur = p._tur, komediYonetmen = p._yonetmen })
+    .OrderBy(p => p.komediDiziAdi)
+    .ThenBy(p => p.komediYonetmen);
+foreach (var e in DiziFinal) Console.WriteLine("{0}, {1}, {2}",e.komediDiziAdi,e.komediYonetmen,e.komediTur);
